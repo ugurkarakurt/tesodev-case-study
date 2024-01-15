0abbb7d62cd89d041a0520e135b30a0cd6b20cf0
ugurkarakurt <ugurkarakurt57@gmail.com>
2024-01-15 09:17:57 +0300

    Feat - #1 | Add Form > Title updated.

--- src/components/add-form/add-form.component.jsx
+++ src/components/add-form/add-form.component.jsx
@@ -32,7 +32,7 @@ const AddFormComponent = () => {
       showAlert({
         isShow: true,
         alertType: 'danger',
-        title: "Validation Error.",
+        title: "Validatasdsadasdsadasdadsasdion Error.",
         description: { ...validationErrors },
         buttonContent: 'Error',
       });
